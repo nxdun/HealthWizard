50d620a9746d00c1ed981f6b7914369e70332344
Sathira Dissanayaka <sathiradissanayaka45@gmail.com>
2024-10-19 19:56:09 +0530

    healthcare manager registration

--- backend/models/healthcareManager.js
+++ backend/models/healthcareManager.js
@@ -1,22 +1,31 @@
-const mongoose = require('mongoose');
-const Person = require('./person'); // Importing Person model
+const mongoose = require("mongoose");
+const Person = require("./person");
 
-const healthcareManagerSchema = new mongoose.Schema({
-    managerID: { type: String, required: true, unique: true }, // Unique manager ID
-    department: { type: String, required: true },
-    contactInfo: { type: String, required: true } // Inheriting contactInfo from Person
+const healthManagerSchema = new mongoose.Schema({
+  HealthManagerID: { type: String, required: true, unique: true },
 });
 
-// Adding createAccount method for HealthcareManager
-healthcareManagerSchema.methods.createAccount = async function () {
-    try {
-        return await this.save(); // Save the Healthcare Manager instance
-    } catch (err) {
-        throw new Error('Error creating account: ' + err.message);
-    }
+// Methods specific to the Patient model
+healthManagerSchema.methods.createAccount = async function () {
+  try {
+    return await this.save();
+  } catch (err) {
+    throw new Error("Error creating account: " + err.message);
+  }
 };
 
-// Creating a discriminator for HealthcareManager
-const HealthcareManager = Person.discriminator('HealthcareManager', healthcareManagerSchema);
+// healthManagerSchema.methods.updateAccount = async function (updateData) {
+//   try {
+//     Object.assign(this, updateData);
+//     return await this.save();
+//   } catch (err) {
+//     throw new Error("Error updating account: " + err.message);
+//   }
+// };
 
-module.exports = HealthcareManager;
+// healthManagerSchema.methods.viewMedicalRecords = function () {
+//   return this.medicalHistory;
+// };
+
+const Health = Person.discriminator("Health", healthManagerSchema);
+module.exports = Health;
